5b368135db6ad13ccf3b8d7c2bb152604c6114ba
crynobone <crynobone@gmail.com>
2013-12-20 18:55:27 +0800

    Add some comment.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- config.sh
+++ config.sh
@@ -1,7 +1,11 @@
+# Should always give priority to homebrew.
 PATH=/usr/local/sbin:/usr/local/bin:$PATH
 
+# NPM
 PATH=$PATH:/usr/local/share/npm/bin
 
+# PHP (managed via homebrew)
 PATH=$PATH:/usr/local/Cellar/php54/5.4.22/bin:/usr/local/Cellar/php54/5.4.21/bin
 
+# RVM and Ruby
 PATH=$PATH:$HOME/.rvm/bin:$HOME/.rvm/gems/ruby-1.9.3-p448/bin 
