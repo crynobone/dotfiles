e0c72799b2dd4c94c4ba6e5c98c8cdd76c152f22
crynobone <crynobone@gmail.com>
2014-01-01 22:23:57 +0800

    Remove redundant line.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- config.sh
+++ config.sh
@@ -10,6 +10,5 @@ PATH=$PATH:/usr/local/share/npm/bin
 # PHP (managed via homebrew)
 PATH=$PATH:$(brew --prefix josegonzalez/php/php54)/bin
 
-
 # RVM and Ruby
 PATH=$PATH:$HOME/.rvm/bin:$HOME/.rvm/gems/ruby-1.9.3-p448/bin 
