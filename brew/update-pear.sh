1f38e699fef69f03a968b9ea184a3346fc117e5d
crynobone <crynobone@gmail.com>
2014-01-11 14:17:47 +0800

    Add setup to install phpdoc using pear.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- brew/update-pear.sh
+++ brew/update-pear.sh
@@ -1,5 +1,7 @@
 # A basic bash script to setup phpunit after upgrading PHP.
 sudo pear config-set auto_discover 1
+sudo pear channel-discover pear.phpdoc.org
 sudo pear install pear.phpqatools.org/phpqatools
+sudo pear install phpdoc/phpDocumentor
 
 echo "Please update your PATH to $(brew --prefix josegonzalez/php/php54)"
