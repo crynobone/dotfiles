d508e58d3da027a2483aa4494440b4dda019cb8c
crynobone <crynobone@gmail.com>
2014-01-26 19:00:06 +0800

    Export path to current build.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/nginx-create.sh
+++ bin/nginx-create.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-
+export PATH=/bin:/usr/bin:/usr/local/bin:$PATH
 NAME=$1
 
 # Get target project path
@@ -13,26 +13,26 @@ fi
 
 # Register available path
 TARGET=$DIR
-DIR=`echo $DIR | /usr/local/bin/gsed 's/[]()$*^|[]/\\&/g'`
-DIR=`echo $DIR | /usr/local/bin/gsed 's/[]\/|[]/{h}/g'`
-DIR=`echo $DIR | /usr/local/bin/gsed 's/[].|[]/{d}/g'`
-SOURCE=`/usr/bin/dirname $0`/stub/nginx.site
+DIR=`echo $DIR | gsed 's/[]()$*^|[]/\\&/g'`
+DIR=`echo $DIR | gsed 's/[]\/|[]/{h}/g'`
+DIR=`echo $DIR | gsed 's/[].|[]/{d}/g'`
+SOURCE=`dirname $0`/stub/nginx.site
 CONF=/usr/local/etc/nginx/sites-available/$NAME.dev.conf
 ENABLEDCONF=/usr/local/etc/nginx/sites-enabled/$NAME.dev.conf
 
 for FILE in $ENABLEDCONF $CONF
 do
 	if [ -f $FILE ]; then
-		/bin/rm $FILE
+		rm $FILE
 	fi
 done
 
-/bin/cp $SOURCE $CONF
-/usr/local/bin/gsed -i "s/{name}/$NAME/g" $CONF
-/usr/local/bin/gsed -i "s/{path}/$DIR/g" $CONF
-/usr/local/bin/gsed -i "s/{h}/\//g" $CONF
-/usr/local/bin/gsed -i "s/{d}/\./g" $CONF
-/bin/ln -s $CONF $ENABLEDCONF
+cp $SOURCE $CONF
+gsed -i "s/{name}/$NAME/g" $CONF
+gsed -i "s/{path}/$DIR/g" $CONF
+gsed -i "s/{h}/\//g" $CONF
+gsed -i "s/{d}/\./g" $CONF
+ln -s $CONF $ENABLEDCONF
 
 echo "Configuration: $ENABLEDCONF"
 echo "Hostname: $NAME.dev"
