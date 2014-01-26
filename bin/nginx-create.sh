9ecd7a4d3ed29219c5f35b9161213c683f9bb4fd
crynobone <crynobone@gmail.com>
2014-01-26 18:55:07 +0800

    Some changes
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/nginx-create.sh
+++ bin/nginx-create.sh
@@ -2,6 +2,7 @@
 
 NAME=$1
 
+# Get target project path
 DIR=`pwd`
 if [ -f $DIR/public/index.php ]; then
     DIR=$DIR/public
@@ -10,6 +11,7 @@ if [ -f $DIR/public_html/index.php ]; then
     DIR=$DIR/public_html
 fi
 
+# Register available path
 TARGET=$DIR
 DIR=`echo $DIR | /usr/local/bin/gsed 's/[]()$*^|[]/\\&/g'`
 DIR=`echo $DIR | /usr/local/bin/gsed 's/[]\/|[]/{h}/g'`
@@ -17,22 +19,22 @@ DIR=`echo $DIR | /usr/local/bin/gsed 's/[].|[]/{d}/g'`
 SOURCE=`/usr/bin/dirname $0`/stub/nginx.site
 CONF=/usr/local/etc/nginx/sites-available/$NAME.dev.conf
 ENABLEDCONF=/usr/local/etc/nginx/sites-enabled/$NAME.dev.conf
-echo "Let's get started."
 
 for FILE in $ENABLEDCONF $CONF
 do
 	if [ -f $FILE ]; then
-		/bin/rm $FILE;
+		/bin/rm $FILE
 	fi
 done
 
-/bin/cp $SOURCE $CONF;
-/usr/local/bin/gsed -i "s/{name}/$NAME/g" $CONF;
-/usr/local/bin/gsed -i "s/{path}/$DIR/g" $CONF;
-/usr/local/bin/gsed -i "s/{h}/\//g" $CONF;
-/usr/local/bin/gsed -i "s/{d}/\./g" $CONF;
-/bin/ln -s $CONF $ENABLEDCONF;
-echo "Configuration: $ENABLEDCONF";
-echo "Hostname: $NAME.dev";
-echo "Path webroot: $TARGET";
+/bin/cp $SOURCE $CONF
+/usr/local/bin/gsed -i "s/{name}/$NAME/g" $CONF
+/usr/local/bin/gsed -i "s/{path}/$DIR/g" $CONF
+/usr/local/bin/gsed -i "s/{h}/\//g" $CONF
+/usr/local/bin/gsed -i "s/{d}/\./g" $CONF
+/bin/ln -s $CONF $ENABLEDCONF
+
+echo "Configuration: $ENABLEDCONF"
+echo "Hostname: $NAME.dev"
+echo "Path webroot: $TARGET"
 echo "please restart nginx"
