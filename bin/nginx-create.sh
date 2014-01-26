a6074d1ea1bee6e7b3191fac10602a69bca55638
crynobone <crynobone@gmail.com>
2014-01-26 18:23:36 +0800

    Customize build script.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/nginx-create.sh
+++ bin/nginx-create.sh
@@ -10,7 +10,10 @@ if [ -f $DIR/public_html/index.php ]; then
     DIR=$DIR/public_html
 fi
 
-DIR=`echo $DIR | /usr/local/bin/gsed 's/[]\/()$*.^|[]/\\&/g'`
+TARGET=$DIR
+DIR=`echo $DIR | /usr/local/bin/gsed 's/[]()$*^|[]/\\&/g'`
+DIR=`echo $DIR | /usr/local/bin/gsed 's/[]\/|[]/{h}/g'`
+DIR=`echo $DIR | /usr/local/bin/gsed 's/[].|[]/{d}/g'`
 SOURCE=`/usr/bin/dirname $0`/stub/nginx.site
 CONF=/usr/local/etc/nginx/sites-available/$NAME.dev.conf
 ENABLEDCONF=/usr/local/etc/nginx/sites-enabled/$NAME.dev.conf
@@ -26,8 +29,10 @@ done
 /bin/cp $SOURCE $CONF;
 /usr/local/bin/gsed -i "s/{name}/$NAME/g" $CONF;
 /usr/local/bin/gsed -i "s/{path}/$DIR/g" $CONF;
-/usr/local/bin/gsed -i "s/{path}/\//g" $CONF;
+/usr/local/bin/gsed -i "s/{h}/\//g" $CONF;
+/usr/local/bin/gsed -i "s/{d}/\./g" $CONF;
 /bin/ln -s $CONF $ENABLEDCONF;
-echo "$ENABLEDCONF created";
-/usr/bin/vim $CONF;
+echo "Configuration: $ENABLEDCONF";
+echo "Hostname: $NAME.dev";
+echo "Path webroot: $TARGET";
 echo "please restart nginx"
