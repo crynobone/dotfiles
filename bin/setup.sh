a5d0d049de645098e478667ef3e9f166e8f8db4d
crynobone <crynobone@gmail.com>
2014-02-05 09:19:07 +0800

    Add gem and npm install.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,3 +2,13 @@
 
 # Setup vimrc
 echo "source ~/.dotfiles/vim/vimrc" > $HOME/.vimrc
+
+# Setup basic Gem
+gem install guard
+gem install guard-phpunit2
+gem install mailcatcher
+gem install watson-ruby
+
+# Setup basic Node.js
+npm install -g less
+npm install -g gulp
