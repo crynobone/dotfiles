efb085f48ba71e7325b0ea20c9b3c698772b1f77
crynobone <crynobone@gmail.com>
2014-02-24 09:51:53 +0800

    Show version after install
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/setup-php.sh
+++ bin/setup-php.sh
@@ -12,13 +12,16 @@ mv gush.phar /usr/local/bin/gush
 wget http://codeception.com/codecept.phar
 chmod +x codecept.phar
 mv codecept.phar /usr/local/bin/codecept
+composer -V
 
 # Setup Laravel
 wget http://laravel.com/laravel.phar
 chmod +x laravel.phar
 mv laravel.phar /usr/local/bin/laravel
+laravel -V
 
 # Setup Envoy
 wget https://github.com/laravel/envoy/raw/master/envoy.phar
 chmod +x envoy.phar
 mv envoy.phar /usr/local/bin/envoy
+envoy -V
