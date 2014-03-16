9a2ac0ab5852e0b5c47f6dfb04c526d751710b2d
crynobone <crynobone@gmail.com>
2014-03-16 11:23:26 +0800

    Remove pear.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- brew/update-pear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# A basic bash script to setup phpunit after upgrading PHP.
-pear config-set auto_discover 1
-pear channel-discover pear.phpdoc.org
-pear install pear.phpqatools.org/phpqatools
-pear install phpdoc/phpDocumentor
-
-echo "Please update your PATH to $(brew --prefix josegonzalez/php/php55)"
