4dafd69d8b198591c9dfd289dc14b3b215348761
crynobone <crynobone@gmail.com>
2014-11-21 12:20:29 +0800

    Update composer global setup script.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/setup/composer.sh
+++ bin/setup/composer.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-REQUIRE=("laravel/envoy=~1" "gushphp/gush=*" "laravel/liferraft=~1")
+REQUIRE=("laravel/envoy=~1" "gushphp/gush=*" "illuminate/support=~5.0@dev" "illuminate/contracts=~5.0@dev")
 REQUIREDEV=("pdepend/pdepend=~1" "phpmd/phpmd=~1" "covex-nn/phpcb=~1" "sebastian/phpcpd=~2" "squizlabs/php_codesniffer=~1" "phploc/phploc=~2" "codeception/codeception=~2" "phpdocumentor/phpdocumentor=~2")
 
 cd ~/.composer
