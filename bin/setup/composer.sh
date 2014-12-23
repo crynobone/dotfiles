25eb73141f1720fc615f4b35478ae46347e370b5
crynobone <crynobone@gmail.com>
2014-12-23 13:29:04 +0800

    Update composer build setup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/setup/composer.sh
+++ bin/setup/composer.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-REQUIRE=("laravel/envoy=~1" "gushphp/gush=*" "illuminate/support=~5.0@dev" "illuminate/contracts=~5.0@dev")
-REQUIREDEV=("pdepend/pdepend=~1" "phpmd/phpmd=~1" "covex-nn/phpcb=~1" "sebastian/phpcpd=~2" "squizlabs/php_codesniffer=~1" "phploc/phploc=~2" "codeception/codeception=~2" "phpdocumentor/phpdocumentor=~2")
+REQUIRE=("laravel/envoy=~1" "halleck45/phpmetrics=~1.1" "illuminate/support=~5.0@dev" "illuminate/contracts=~5.0@dev")
+REQUIREDEV=("pdepend/pdepend=~1" "phpmd/phpmd=~1" "covex-nn/phpcb=~1" "sebastian/phpcpd=~2" "squizlabs/php_codesniffer=~1" "phploc/phploc=~2" "codeception/codeception=~2" "phpdocumentor/phpdocumentor=~2" "phpspec/phpspec=~2.1")
 
 cd ~/.composer
 composer global require $REQUIRE --prefer-dist -vvv
