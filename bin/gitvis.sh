966f885858773a36ff745b2b7783363e6645f79d
crynobone <crynobone@gmail.com>
2015-03-01 21:39:14 +0800

    Add default size.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/gitvis.sh
+++ bin/gitvis.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
+DIMENSION=${1:-1280x720}
 
-gource --auto-skip-seconds 1 --seconds-per-day 1 --bloom-multiplier 1.2 --bloom-intensity 1.2 --background 2F3030 -e 0.1 --max-files 0 --max-file-lag 1 --multi-sampling -1280x720 -o gource.ppm --user-image-dir .git/avatar/
+gource --auto-skip-seconds 1 --seconds-per-day 1 --bloom-multiplier 1.2 --bloom-intensity 1.2 --background 2F3030 -e 0.1 --max-files 0 --max-file-lag 1 --multi-sampling -$DIMENSION -o gource.ppm --user-image-dir .git/avatar/
