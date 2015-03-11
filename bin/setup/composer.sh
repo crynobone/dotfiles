73c088c65c5b68133c92bf3d68a48bd91ae98a90
crynobone <crynobone@gmail.com>
2015-03-11 22:45:17 +0800

    Update setup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- bin/setup/composer.sh
+++ bin/setup/composer.sh
@@ -4,5 +4,5 @@ REQUIRE=("laravel/envoy=~1" "halleck45/phpmetrics=~1.1" "illuminate/support=~5.0
 REQUIREDEV=("pdepend/pdepend=~1" "phpmd/phpmd=~1" "covex-nn/phpcb=~1" "sebastian/phpcpd=~2" "squizlabs/php_codesniffer=~1" "phploc/phploc=~2" "codeception/codeception=~2" "phpdocumentor/phpdocumentor=~2" "phpspec/phpspec=~2.1")
 
 cd ~/.composer
-composer global require $REQUIRE --prefer-dist -vvv
-composer global require $REQUIREDEV --prefer-dist -vvv
+composer global require $REQUIRE --prefer-dist -vvv --profile
+composer global require $REQUIREDEV --prefer-dist -vvv --profile
--- bin/setup/php.sh
+++ bin/setup/php.sh
@@ -6,14 +6,3 @@ chmod +x composer.phar
 mv composer.phar /usr/local/bin/composer
 composer -V
 
-# Setup Laravel
-wget http://laravel.com/laravel.phar
-chmod +x laravel.phar
-mv laravel.phar /usr/local/bin/laravel
-laravel -V
-
-# Setup Envoy
-wget https://github.com/laravel/envoy/raw/master/envoy.phar
-chmod +x envoy.phar
-mv envoy.phar /usr/local/bin/envoy
-envoy -V
