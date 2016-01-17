4976350b81bdea9c46a715351ead5f75dbd413a7
crynobone <crynobone@gmail.com>
2016-01-17 16:45:10 +0800

    Cleanup unused aliases.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- oh-my-zsh/aliases.sh
+++ oh-my-zsh/aliases.sh
@@ -6,11 +6,7 @@ alias env-update="sh $DOTFILES/bin/update.sh"
 # Vagrant
 alias vm="ssh vagrant@127.0.0.1 -p 2222"
 alias vaprobash="wget -O Vagrantfile http://bit.ly/vaprobash"
-alias vagrant-cid="vagrant ssh -c 'cd /vagrant && composer install --prefer-dist'"
-alias vagrant-cud="vagrant ssh -c 'cd /vagrant && composer update --prefer-dist'"
-alias vagrant-optimize="vagrant ssh -c 'php /vagrant/artisan orchestra:optimize --force'"
-alias vagrant-clear="vagrant ssh -c 'php /vagrant/artisan cache:clear'"
-alias vagrant-assemble="vagrant ssh -c 'php /vagrant/artisan orchestra:assemble'"
+
 # Nginx alias
 alias nginx-start="launchctl start homebrew.mxcl.nginx && echo 'Nginx started'"
 alias nginx-stop="launchctl stop homebrew.mxcl.nginx && echo 'Nginx stopped'"
@@ -71,12 +67,11 @@ alias vc="open ./build/coverage/index.html"
 
 # General
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard.'"
-alias mailcatch="mailcatcher && sudo postfix start"
-alias hipchat-please="sudo /Applications/HipChat.app/Contents/MacOS/HipChat > /dev/null 2>&1"
 alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias csfixer="php-cs-fixer fix"
 alias csfixer-test="php-cs-fixer fix tests/ --level=psr2 --fixers=-psr0,align_double_arrow,concat_without_spaces,double_arrow_multiline_whitespaces,duplicate_semicolon,empty_return,extra_empty_lines,include,join_function,multiline_array_trailing_comma,new_with_braces,no_blank_lines_after_class_opening,no_empty_lines_after_phpdocs,object_operator,operators_spaces,phpdoc_indent,phpdoc_separation,phpdoc_short_description,phpdoc_to_comment,phpdoc_trim,phpdoc_type_to_var,phpdoc_var_without_name,single_array_no_trailing_comma,remove_leading_slash_use,remove_lines_between_uses,short_array_syntax,spaces_cast,standardize_not_equal,unalign_double_arrow,unalign_equals,unused_use,whitespacy_lines,visibility"
 alias eject="/usr/bin/drutil eject"
+
 # Be nice
 alias please="sudo"
 alias kaiju="su - root"
